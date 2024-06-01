188961c8352ded1090e2f4feb156d887d6f95597
Nikita Savin <klaster26@mail.ru>
2024-06-01 22:31:44 +0300

    feat: реализовать метод обхода и парсинга набора страниц

--- serbian_lines_scrapper/routes_scrapper.py
+++ serbian_lines_scrapper/routes_scrapper.py
@@ -74,6 +74,13 @@ class RoutesScrapper(BelgradTrasnportCrawler):
         super().__init__(host, headers)
 
     def __get_schedule__(self, table_obj):
+
+        def save_from_empty(string):
+            newstring = string.strip()
+            if newstring == "":
+                return None
+            return newstring
+
         # Для визуального удобства в заголовке таблицы первый и последний
         # столбцы - это часы отправления, но нам они здесь не нужны.
         table_head_cells = table_obj.thead.find_all("th")[1:-1]
@@ -98,11 +105,27 @@ class RoutesScrapper(BelgradTrasnportCrawler):
             # неиндексируемый, поэтому превратим каждую строку
             # из совокупности ячеек во вполне конкретный список, заодно
             # убрав ячейку со служебной информацией (с атрибутом colspan)
+
+            # Как красиво это задумывалось
+            """
             td_tags = [
                 td_tag
                 for td_tag in tr_tag
-                if td_tag.text != "\n" and "colspan" not in td_tag.attrs
-            ]
+                #if td_tag.text != "\n" and
+                if "colspan" not in td_tag.attrs
+            ]"""
+
+            # И как в итоге это получилось
+
+            td_tags = []
+            for td_tag in tr_tag:
+                try:
+                    if "colspan" not in td_tag.attrs:
+                        continue
+                except AttributeError:
+                    continue
+                td_tags.append(td_tag)
+
             # Практического смысла в отдельной переменной для range
             # никакого. Просто если дальше его вставлять в генератор
             # списка, то длина строки превысит численность Индии.
@@ -117,19 +140,39 @@ class RoutesScrapper(BelgradTrasnportCrawler):
             # strip() какое-то запредельное, но одним только фронтендерам
             # известно, сколько в результате в верстке оказалось лишних
             # пробелов, табуляций и переносов строки.
+
+            # Как красиво это задумывалось
+            """
             departure_minutes = [
                 list(
                     map(
                         # Сэкономим немножко времени, добавим час
                         # отправления из первого столбца к каждому
                         # значению минут.
-                        lambda x: td_tags[0].text + ":" + x.strip(),
+                        lambda x: td_tags[0].text + ":" + save_from_empty(x),
                         td_tags[i].text.strip().split("\n"),
                     )
                 )
                 for i in range_generator
             ]
+            """
 
+            # И как в итоге это получилось
+            departure_minutes = []
+            for i in range_generator:
+                splittes_minutes = td_tags[i].text.strip().split("\n")
+                if len(splittes_minutes) == 0:
+                    departure_minutes.append(None)
+                    continue
+                for y in range(len(splittes_minutes)):
+                    proc_minutes = save_from_empty(splittes_minutes[y])
+                    if proc_minutes is not None:
+                        splittes_minutes[y] = (
+                            td_tags[0].text + ":" + proc_minutes
+                        )
+                    else:
+                        splittes_minutes[y] = None
+                departure_minutes.append(splittes_minutes)
             # В результате мы получили список списков, где каждый список
             # соответствует столбцу из заголовка.
             # Проверка, что все прошло успешно - утверждение, что длина
@@ -161,18 +204,47 @@ class RoutesScrapper(BelgradTrasnportCrawler):
         soup = self.get_bs_object(path=link)
         route_name = soup.h1.get_text(strip=True)
         h2_headers = soup.find_all("h2")
-        description, first_station, dbl_description, last_station = map(
-            lambda tag: tag.get_text(strip=True), h2_headers
-        )
+        if len(h2_headers) == 2:
+            description, first_station = map(
+                lambda tag: tag.get_text(strip=True), h2_headers
+            )
+
+        else:
+            description, first_station, dbl_description, last_station = map(
+                lambda tag: tag.get_text(strip=True), h2_headers
+            )
+            route.last_station = last_station
+
         route.route_name = route_name
         route.description = description
         route.first_station = first_station
-        route.last_station = last_station
 
         # На странице находятся две таблицы: по одной со временем отправления
         # из каждой конечной точки. Получим эти две таблицы, чтобы было удобно
         # с ними работать, и каждую таблицу передадим в функцию.
-        table_first, table_last = soup.find_all("table")
-        route.first_st_dep = self.__get_schedule__(table_first)
-        route.last_st_dep = self.__get_schedule__(table_last)
+        schedule_tables = soup.find_all("table")
+        if len(schedule_tables) == 1:
+            table = schedule_tables[0]
+            route.first_st_dep = self.__get_schedule__(table)
+        else:
+            table_first, table_last = soup.find_all("table")
+            route.first_st_dep = self.__get_schedule__(table_first)
+            route.last_st_dep = self.__get_schedule__(table_last)
         return route
+
+    def parse_all_routes(self, path, clear_from_pdf):
+        links_to_crawl = self.collect_links_to_crawl(
+            path, "option", clear_from_pdf=clear_from_pdf
+        )
+        parsed_objects = []
+        counter = len(links_to_crawl)
+        for link in links_to_crawl:
+            print(f"Перехожу к ссылке {link}. Осталось {counter - 1} ссылок.")
+            try:
+                parsed_objects.append(self.parse_route(link))
+            except Exception as e:
+                msg = f"Problem with link {link}. "
+                raise MyBSException(msg + str(e))
+            finally:
+                counter -= 1
+        return parsed_objects
